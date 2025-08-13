daec8c31ca8fd105d45744c1f7e93b0a6cda0cce
JuanGualim <gua24852@uvg.edu.gt>
2025-08-12 23:34:15 -0600

    Agregación de cards para mostras las tareas

--- app/src/main/java/com/example/laboratorio3_pm/MainActivity.kt
+++ app/src/main/java/com/example/laboratorio3_pm/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.laboratorio3_pm
 
+import android.os.AsyncTask
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
@@ -9,6 +10,7 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
@@ -17,11 +19,19 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Check
 import com.example.laboratorio3_pm.ui.theme.Laboratorio3PMTheme
 
+data class Task(val text: String, val done: Boolean = false)
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -59,7 +69,8 @@ fun PantallaPrincipal(
                 modifier = Modifier.padding(16.dp),
                 style = MaterialTheme.typography.headlineLarge,
                 fontWeight = FontWeight.Bold,
-                color = Color(0xFF254C4A)
+                color = Color(0xFF254C4A),
+                fontFamily = FontFamily.Serif
             )
 
             Spacer(modifier = Modifier.height(20.dp))
@@ -71,10 +82,53 @@ fun PantallaPrincipal(
 }
 
 @Composable
+fun TaskItem(
+    task: Task,
+    onComplete: () -> Unit,
+    onDelete: () -> Unit,
+){
+    Card (
+        colors = CardDefaults.cardColors(containerColor = Color.White),
+        modifier = Modifier.fillMaxWidth()
+    ){
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 12.dp, vertical = 10.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ){
+            Text(
+                text = task.text,
+                color = Color.Black,
+                style = TextStyle(
+                    fontSize = 18.sp,
+                    textDecoration = if (task.done) TextDecoration.LineThrough else TextDecoration.None
+                ),
+                modifier = Modifier.weight(1f)
+            )
+
+            IconButton(onClick = onComplete) {
+                Icon(
+                    imageVector = Icons.Filled.Check,
+                    contentDescription = stringResource(R.string.completar_tarea),
+                    tint = Color(0xFF4CAF50)
+                )
+            }
+            IconButton(onClick = onDelete) {
+                Icon(
+                    imageVector = Icons.Filled.Close,
+                    contentDescription = stringResource(R.string.eliminar_tarea),
+                    tint = Color(0xFFD32F2F)
+                )
+            }
+        }
+    }
+}
+@Composable
 fun ListaTareas() {
     val context = LocalContext.current
     var nuevaTarea by remember { mutableStateOf("") }
-    val tareas = remember { mutableStateListOf<String>() }
+    val tareas = remember { mutableStateListOf<Task>() }
 
     Column(
         modifier = Modifier.fillMaxSize(),
@@ -86,8 +140,17 @@ fun ListaTareas() {
                 .weight(1f),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            items(tareas) { tarea ->
-                Text(text = tarea, modifier = Modifier.padding(8.dp))
+            itemsIndexed(tareas) { index, tarea ->
+                TaskItem(
+                    task = tarea,
+                    onComplete = {
+                        tareas[index] = tareas[index].copy(done = true)
+                    },
+                    onDelete= {
+                        tareas.removeAt(index)
+                    }
+                )
+                Spacer(modifier = Modifier.height(8.dp))
             }
         }
 
@@ -106,7 +169,7 @@ fun ListaTareas() {
             Spacer(modifier = Modifier.width(8.dp))
             Button(onClick = {
                 if (nuevaTarea.isNotBlank()) {
-                    tareas.add(nuevaTarea)
+                    tareas.add(Task(nuevaTarea))
                     nuevaTarea = ""
                 } else {
                     Toast.makeText(
